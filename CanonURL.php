8a4de5fb930ee70a070059a765614fb9d8a76565
Timo Tijhof <krinklemail@gmail.com>
2013-03-14 19:26:27 +0100

    Clean up, fix #3. Bump version to 0.3.0
    * Indentation and whitespace
    * Prefix global function with ef per MediaWiki convention
    * Use OutputPage::setCanonicalUrl to avoid adding a second
      rel=canonical if MediaWiki core adds one already (fixes #3).
    * Include patch version (".0") per semver guidelines.
    * Prefer HTTPS urls for pages on servers that support it.
    * Fix syntax in README.md to be proper Markdown, it currently
      looks broken when rendered on GitHub.
    * Turn README.md into a symlink instead of duplicating the content.

--- CanonURL.php
+++ CanonURL.php
@@ -1,45 +1,42 @@
-<?php 
+<?php
 
 /**
  * Initialization file for the CanonURL extension.
  *
- * Documentation:	 		http://www.mediawiki.org/wiki/Extension:CanonURL
- * Support					http://www.mediawiki.org/wiki/Extension_talk:CanonURL
- * Source code:             https://github.com/Abhi-M/CanonURL/
+ * Source code:    https://github.com/Abhi-M/CanonURL
+ * Documentation:  https://www.mediawiki.org/wiki/Extension:CanonURL
+ * Support:        https://github.com/Abhi-M/CanonURL/issues
  *
- * @file CanonURL.php
- * @ingroup CanonURL
- *
- * @licence GNU GPL v3
- *
- * @author Abhi M Balakrishnan http://www.mediawiki.org/wiki/User:Abhi_M_Balakrishnan
- */ 
-	if ( !defined( 'MEDIAWIKI' ) ) {
-		echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
-		die( 1 );
-	}
-	
-	define( 'CanonURL_VERSION', '0.2' );
-	
-	$wgExtensionCredits['other'][] = array(
-		'path' => __FILE__,
-		'name' => 'CanonURL',
-		'version' => CanonURL_VERSION,
-		'author' => array( '[http://www.mediawiki.org/wiki/User:Abhi_M_Balakrishnan Abhi M Balakrishnan]' ),
-		'url' => 'https://mediawiki.org/wiki/Extension:CanonURL',
-		'descriptionmsg' => 'canonurl-desc',
-	);
-	
-	$wgExtensionMessagesFiles['CanonURL'] = dirname( __FILE__ ) . '/CanonURL.i18n.php';
-	 
-	global $wgHooks;
-	$wgHooks['BeforePageDisplay'][]  = 'CanonURL';
-	 
-	function CanonURL($out)
-	{
-		$out->addHeadItem( 'canonical',
-			'<link rel="canonical" href="' . htmlspecialchars( $out->getTitle()->getCanonicalURL() ) . '" />' . "\n"
-		);
-		return true;
-	}
+ * @author Abhi M Balakrishnan https://www.mediawiki.org/wiki/User:Abhi_M_Balakrishnan
+ * @license GNU GPL v3
+ * @file
+ * @ingroup Extensions
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	die( 1 );
+}
 
+define( 'CanonURL_VERSION', '0.3.0' );
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'CanonURL',
+	'author' => array( '[https://www.mediawiki.org/wiki/User:Abhi_M_Balakrishnan Abhi M Balakrishnan]' ),
+	'version' => CanonURL_VERSION,
+	'url' => 'https://mediawiki.org/wiki/Extension:CanonURL',
+	'descriptionmsg' => 'canonurl-desc',
+);
+
+$wgExtensionMessagesFiles['CanonURL'] = dirname( __FILE__ ) . '/CanonURL.i18n.php';
+
+$wgHooks['BeforePageDisplay'][]  = 'efCanonURL';
+
+/**
+ * @param OutputPage $out
+ * @param Skin $skin
+ */
+function efCanonURL( $out ) {
+	$out->setCanonicalUrl( $out->getTitle()->getCanonicalURL() );
+	return true;
+}
